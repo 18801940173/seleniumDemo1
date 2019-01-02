94cb1f11d4c1a69cd2129aea50200114529b3ba0
18801940173 <18801940173@163.com>
2019-01-02 14:55:55 +0800

    测试用

--- src/test/java/com/autowebjava/day4/commitTest.java
+++ src/test/java/com/autowebjava/day4/commitTest.java
@@ -1,7 +1,14 @@
 package com.autowebjava.day4;
 
+import org.openqa.selenium.io.TemporaryFilesystem;
+import org.testng.annotations.Test;
+
 /**
  * Created by sundongfeng on 2018/12/29
  */
 public class commitTest {
+    @Test
+    public void test(){
+        System.out.println("测试！");
+    }
 }
