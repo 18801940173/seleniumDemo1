e9a966149ff30a72b19c966efbd562d31011f8a5
18801940173 <18801940173@163.com>
2019-01-03 17:17:41 +0800

    修改

--- src/test/java/com/autowebjava/day1/TestNGDemo1.java
+++ src/test/java/com/autowebjava/day1/TestNGDemo1.java
@@ -16,7 +16,6 @@ public class TestNGDemo1 {
     @BeforeMethod
     public void beforeMethod(){
         System.out.println("这是BeforeMethod注解");
-        Assert.assertNotEquals(2,2);
     }
     @Test
     public void testCase1(){
@@ -25,6 +24,7 @@ public class TestNGDemo1 {
     @Test
     public void testCase2(){
         System.out.println("这是@Test注解,case2");
+        Assert.assertNotEquals(2,2);
     }
     @AfterMethod
     public void afterMethod1(){
